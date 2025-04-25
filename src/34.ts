91228b11eefc5415fa6513ce5631891d526202d9
mellosashmore689 <mellosashmore689@gmail.com>
2025-04-25 09:09:51 +0000

    Add 34.ts

---
+++ src/34.ts
@@ -0,0 +1,11 @@
+import sympy as sp
+
+def solve_system(x, y):
+    # Solve the system of equations
+    solution = sp.solve([x - 2*y, x + 3*y], (x, y))
+    return solution
+
+# Generate random data for the code
+random_data = [5, 10]
+solution_data = solve_system(random_data[0], random_data[1])
+print(solution_data)
