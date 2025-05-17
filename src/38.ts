2877a07980a21049e5a389e89ad0119d48c76e57
mellosashmore689 <mellosashmore689@gmail.com>
2025-05-17 08:51:42 +0000

    Add 38.ts

---
+++ src/38.ts
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calculate_area(radius):
+    """
+    Calculate the area of a circle given its radius.
+    
+    Parameters:
+    - radius (float): The radius of the circle.
+    
+    Returns:
+    - float: The area of the circle.
+    """
+    return np.pi * radius ** 2
